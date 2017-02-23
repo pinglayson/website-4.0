a9916320e2b0e3a0f0ecfa0354315cbe35cc8502
pinglayson <pinglayson@gmail.com>
2017-02-23 14:52:03 +0800

    search functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,32 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filterTodos = todos;
+
+    // Filter by showCompleted
+    filterTodos = filterTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    // Filter by searchText
+    filterTodos = filterTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || ~text.indexOf(searchText.toLowerCase());
+    });
+
+    // Sort todos with non-completed first
+    filterTodos.sort((a, b) => {
+      if(!a.completed && b.completed) {
+        return -1;
+      } else if(a.completed && !b.completed){
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filterTodos;
   }
 };
