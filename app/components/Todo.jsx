6cf3a77c595013d130ad255d7867fa0d2bf88c5b
pinglayson <pinglayson@gmail.com>
2017-02-28 17:17:33 +0800

    fetch todos from firebase on app start

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -25,7 +25,7 @@ export var Todo = React.createClass({
           dispatch(actions.startToggleTodo(id, !completed));
         }}>
         <div>
-          <input type="checkbox" checked={completed}/>
+          <input type="checkbox" defaultChecked={completed}/>
         </div>
         <div>
           <p>{text}</p>
