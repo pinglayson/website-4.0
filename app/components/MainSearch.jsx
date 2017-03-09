a2bfcf5f28a769bb6f05767c7ff67ff66186a670
pinglayson <pinglayson@gmail.com>
2017-03-09 17:48:27 +0800

    styling for search

--- app/components/MainSearch.jsx
+++ app/components/MainSearch.jsx
@@ -3,90 +3,105 @@ import React from 'react';
 export class MainSearch extends React.Component {
   render () {
     return (
-      <div>
-        <div className="show-for-small-only hero-background">
+      <div className="main-search">
+        <div className="hide-for-large">
 
         </div>
-        <div className="hide-for-small-only" data-sticky-container>
-          <div className="hide-for-small-only sticky hero-background" data-sticky data-options="marginTop:3.4;" data-top-anchor="main-content:top" data-btm-anchor="main-content:bottom">
+        <div className="show-for-large" data-sticky-container>
+          <div className="show-for-large sticky hero-background" data-sticky data-options="marginTop:3.4;" data-top-anchor="main-content:top" data-btm-anchor="main-content:bottom">
 
-            <div className="search-container">
-              <div>
-                <h1>The Edege Property. Your home for real estate</h1>
+            <div className="main-search-container row column">
+              <div className="search-container row column">
+                <div className="small-8 small-centered columns text-center">
+                  <h3>The Edge Property. Your home for real estate</h3>
+                </div>
+                <div className="small-11 small-centered columns no-padding-sides">
+                  <div className="search-tabs small-12 columns no-padding-sides">
+                    <div className="buy-rent-toggle small-5 columns">
+                      <div className="small-6 columns">
+                        Buy
+                      </div>
+                      <div className="small-6 columns">
+                        Rent
+                      </div>
+                    </div>
+                    <div className="small-7 columns">
+                      <div className="small-8 columns no-padding-sides">Check Valuation</div>
+                      <div className="small-4 columns">Research</div>
+                    </div>
+                  </div>
+                  <div className="main-search-text">
+                    <input type="text" placeholder="Enter Project, Street Name or Address"/>
+                  </div>
+                  <div className="search-details row extended">
+                    <div className="row extended">
+                      <div className="small-3 columns">
+                        <div className="input-container">
+                          <label>Property Type
+                            <select>
+                              <option>Types</option>
+                            </select>
+                          </label>
+                        </div>
+                      </div>
+                      <div className="small-6 columns">
+                        <div className="input-container">
+                          <label>District
+                            <select>
+                              <option>District</option>
+                            </select>
+                          </label>
+                        </div>
+                      </div>
+                      <div className="small-3 columns">
+                        <div className="input-container">
+                          <label>Bed Rooms
+                            <select>
+                              <option>1</option>
+                            </select>
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row extended">
+                      <div className="double-input-container small-8 columns no-padding-sides">
+                        <div className="small-5 columns">
+                          <label>Price</label>
+                          <select>
+                            <option>Min</option>
+                          </select>
+                        </div>
+                        <div className="small-2 columns text-center">
+                          <i className="fa fa-arrow-right fa-lg" aria-hidden="true"></i>
+                        </div>
+                        <div className="small-5 columns">
+                          <label>&nbsp;</label>
+                          <select>
+                            <option>Max</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="small-4 columns">
+                        <button className="button expanded large">Search</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="search-more-options row extended">
+                    <div className="small-8 columns no-padding-right">
+                      <input ref="belowValuation" type="checkbox" />
+                      <label className="super-no-margin-right">Show me only properties below valuation</label>
+                    </div>
+                    <div className="small-4 columns no-padding-left text-right">
+                      <h5>More Options</h5>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div>
-                <div className="search-tabs">
-                  <div className="buy-rent-toggle">
-                    <div>
-                      <span>Buy</span>
-                    </div>
-                    <div>
-                      <span>Rent</span>
-                    </div>
-                  </div>
-                  <div>
-                    <span>Check Valuation</span><span>Research</span>
-                  </div>
-                </div>
-                <div className="main-search-text">
-                  <input type="text" placeholder="Enter Project, Street Name or Address"/>
-                </div>
-                <div className="search-details">
-                  <div>
-                    <div>
-                      <label>Property Type
-                        <select>
-                          <option>Types</option>
-                        </select>
-                      </label>
-                    </div>
-                    <div>
-                      <label>District
-                        <select>
-                          <option>District</option>
-                        </select>
-                      </label>
-                    </div>
-                    <div>
-                      <label>Bed Rooms
-                        <select>
-                          <option>1</option>
-                        </select>
-                      </label>
-                    </div>
-                  </div>
-                  <div>
-                    <div>
-                      <label>Price
-                        <select>
-                          <option>Min</option>
-                        </select>
-                      </label>
-                      <i className="fa fa-arrow-right fa-lg" aria-hidden="true"></i>
-                      <label>
-                        <select>
-                          <option>Max</option>
-                        </select>
-                      </label>
-                    </div>
-                    <div>
-                      <button>Search</button>
-                    </div>
-                  </div>
-                  <div>
-                    <div>
-                      <input ref="checkbox1" type="checkbox" /><label>Show me only properties below valuation</label>
-                    </div>
-                    <div>
-                      <h2>More Options</h2>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <div className="search-footer">
-              <i className="fa fa-apple fa-lg" aria-hidden="true"></i> Get Edge Property on iOS <i className="fa fa-android fa-lg" aria-hidden="true"></i> Get Edge Property on Android
+              <div className="search-footer">
+                <label>&nbsp;<i className="fa fa-apple fa-lg" aria-hidden="true"></i> Get Edge Property on iOS &nbsp;<i className="fa fa-android fa-lg" aria-hidden="true"></i> Get Edge Property on Android</label>
+              </div>
+
             </div>
 
           </div>
